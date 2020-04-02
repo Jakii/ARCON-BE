84c3bde577c395c1884ed16409cb28d178fba7d9
Jakii <jakii_paz.07@hotmail.es>
2020-04-01 18:29:43 -0600

    Borrando descripcion2

--- ArconApi/Data/Models/Status.cs
+++ ArconApi/Data/Models/Status.cs
@@ -10,7 +10,6 @@ namespace ArconApi.Data.Models
         public int StatusId { get; set; }
         public string StatusName { get; set; }
         public string Description { get; set; }
-        public string Description2 { get; set; }
         public ICollection<Goal> Goals { get; set; }
 
         internal void SetStatusName(string statusName)
